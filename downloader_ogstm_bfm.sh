8d3aaa715651d61e0e817e6c977d49242f9f4625
Giorgio Bolzon <gbolzon@inogs.it>
2017-06-20 17:16:55 +0200

    Version used for V3 qualification run, using tags for ogstm and 3dvar
    V3 qualification run is a 1/24 free-surface run
    HINDCAST: 2014-2016
    RA      : 2015-2016
    
    using simu_v19 INGV forcings

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -4,33 +4,22 @@
   BFM_version=BFMv2   #  BFMv2 or bfmv5
   BFM_RELEASE=branches/new_nutupt
 
- OGSTM_BRANCH=un24_omp_optimized
- VAR3D_BRANCH=most_optimized
+ OGSTM_BRANCH=release-3.0
+ VAR3D_BRANCH=release-3.0
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
-if  [ $BFM_version ==  BFMv2 ] ; then
-
-svn co --username $SVN_USER https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} bfm
-
-else
-    # Requirement: to have an account on git server
-    git clone git@dev.cmcc.it:bfm
-    cd bfm
-    git checkout dev
-fi
+svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 118 bfm
 
 cd $OGSTM_HOME
-#svn co --username $SVN_USER https://hpc-forge.cineca.it/svn/ogstm/${OGSTM_RELEASE} ogstm
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
-git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
+git checkout -b $OGSTM_BRANCH $OGSTM_BRANCH
 
 cd $OGSTM_HOME
-# svn co https://hpc-forge.cineca.it/svn/opa_rea/DA/src/3d_var 
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
+git checkout -b $VAR3D_BRANCH $VAR3D_BRANCH
