09aed34db16d885021ab6e6abe921ea2f6b0fe7b
Giorgio Bolzon <gbolzon@inogs.it>
2017-06-28 13:51:27 +0200

    Using version with snutell in 3dvar

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -5,7 +5,7 @@
   BFM_RELEASE=branches/new_nutupt
 
  OGSTM_BRANCH=un24_omp_optimized
- VAR3D_BRANCH=release-3.0
+ VAR3D_BRANCH=first_attempt
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
@@ -18,9 +18,8 @@ cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
 git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
-git checkout c67940a4a842d579116ca27a369beb85d38b93ec
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
-git checkout -b $VAR3D_BRANCH $VAR3D_BRANCH
+git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
