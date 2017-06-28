54b9bcd71a8ea6ddf05ac951c3888338da875fef
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2017-06-28 13:34:33 +0200

    Version used for Wrong (= without hard_tissue_pump) eas2_v19_2
    It uses a previous commit, in order to use ogstm/snutell.f90
    because 3dvar version does not integrate it yet.

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -4,7 +4,7 @@
   BFM_version=BFMv2   #  BFMv2 or bfmv5
   BFM_RELEASE=branches/new_nutupt
 
- OGSTM_BRANCH=release-3.0
+ OGSTM_BRANCH=un24_omp_optimized
  VAR3D_BRANCH=release-3.0
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
@@ -17,7 +17,8 @@ svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 118 bfm
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
-git checkout -b $OGSTM_BRANCH $OGSTM_BRANCH
+git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
+git checkout c67940a4a842d579116ca27a369beb85d38b93ec
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
