ba54ec7b659d39c039233647c2f2eb1b94db8dd3
Giorgio Bolzon <gbolzon@inogs.it>
2017-07-05 09:04:13 +0200

    Start of Somot setup for 100 years clim simulations

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -4,8 +4,7 @@
   BFM_version=BFMv2   #  BFMv2 or bfmv5
   BFM_RELEASE=branches/new_nutupt
 
- OGSTM_BRANCH=un24_omp_optimized
- VAR3D_BRANCH=WithVb
+ OGSTM_BRANCH=Somot
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
@@ -19,7 +18,3 @@ git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
 git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
 
-cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
-cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
