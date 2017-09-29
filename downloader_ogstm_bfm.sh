d2f0ced47791ec8ba936dea85fc49f302bb18477
Giorgio Bolzon <gbolzon@inogs.it>
2017-09-29 11:29:30 +0200

    Operational Chain Copernicus V3.1 settings, downloader uses only tags

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -2,25 +2,25 @@
 
 
   BFM_version=BFMv2   #  BFMv2 or bfmv5
-  BFM_RELEASE=branches/new_nutupt
+  BFM_RELEASE=tags/release-3.1/
 
- OGSTM_BRANCH=master
- VAR3D_BRANCH=WithVb
+ OGSTM_BRANCH=release-3.1
+ VAR3D_BRANCH=release-3.1
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
-svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 119 bfm
+svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 120 bfm
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
-#git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH #unuseful if branch is master
+git checkout -b $OGSTM_BRANCH $OGSTM_BRANCH
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
+git checkout -b $VAR3D_BRANCH $VAR3D_BRANCH
 
