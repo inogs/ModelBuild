234c400879b6ad01f894ba6e157665fc7827f16a
Giorgio Bolzon <gbolzon@inogs.it>
2018-04-06 15:18:50 +0200

    Automatic choice of OGSTM_BUILD dir

--- builder_ogstm_bfm.sh
+++ builder_ogstm_bfm.sh
@@ -136,15 +136,16 @@ if [ $CMAKE -eq 1 ] ; then
   # ------------ new OGSTM builder based on cmake -----------------
         if [[ $DEBUG == .dbg ]] ; then
            CMAKE_BUILD_TYPE=Debug
+           OGSTM_BLD_DIR=OGSTM_BUILD_DBG
         else
            CMAKE_BUILD_TYPE=Release
+           OGSTM_BLD_DIR=OGSTM_BUILD
         fi
 	export BFM_INCLUDE=$BFM_INC
 	export BFM_LIBRARY=$BFM_LIB
 	export NETCDFF_LIB=$NETCDFF_LIB/libnetcdff.so
 	export NETCDF_LIB=$NETCDF_LIB/libnetcdf.so
 	
-	OGSTM_BLD_DIR=OGSTM_BUILD
 	mkdir -p $OGSTM_BLD_DIR
 	cd $OGSTM_BLD_DIR
 
