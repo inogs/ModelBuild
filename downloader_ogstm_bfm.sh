89e34e66f02a72717b7fdb783f8cf3c09aa0a389
Giorgio Bolzon <gbolzon@inogs.it>
2018-06-18 16:00:45 +0200

    Now we use master, ogstm mfs16_degradation branch is obsolete

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -4,7 +4,7 @@
   BFM_version=BFMv2   #  BFMv2 or bfmv5
   BFM_RELEASE=branches/new_nutupt
 
- OGSTM_BRANCH=mfs16_degradation
+ OGSTM_BRANCH=master
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
@@ -23,6 +23,6 @@ fi
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
-cd ogstm
-git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
+#cd ogstm
+#git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
 
