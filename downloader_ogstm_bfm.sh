29eb90f6510528cc4d8c22531201debe9778df33
Giorgio Bolzon <gbolzon@inogs.it>
2018-11-23 09:59:26 +0100

    Creating new activity Dardanelles

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -2,32 +2,22 @@
 
 
   BFM_version=bfmv5   #  BFMv2 or bfmv5
-  BFM_RELEASE=branches/pl_mod 
+  BFM_BRANCH=dev_ogs
 
- OGSTM_BRANCH=master
+ OGSTM_BRANCH=bc_refactoring
  VAR3D_BRANCH=WithVb
 
-SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
-if  [ $BFM_version ==  BFMv2 ] ; then
-
-svn co --username $SVN_USER https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} bfm
-
-else
-    # Requirement: to have an account on git server
-    git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
-    cd bfm
-    git checkout -b dev_ogs origin/dev_ogs
-fi
+# Requirement: to have an account on git server
+git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
+cd bfm
+git checkout -b dev_ogs origin/dev_ogs
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
+cd ogstm
+git checkout -b $OGSTM_BRANCH -b $OGSTM_BRANCH
 
-
-cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
-cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
