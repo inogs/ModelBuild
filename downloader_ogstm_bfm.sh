566c74a00edb421d88d89930026690f1b86e2877
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2019-03-06 14:37:31 +0100

    Setting V5C releases of code

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
 
-  BFM_version=BFMv2   #  BFMv2 or bfmv5
-  BFM_RELEASE=tags/release-3.1/
+  BFM_version=bfmv5   #  BFMv2 or bfmv5
+  BFM_RELEASE=ogs_release-5.0
 
- OGSTM_BRANCH=release-3.2
- VAR3D_BRANCH=release-3.2
+ OGSTM_BRANCH=release-4.0
+ VAR3D_BRANCH=release-3.3
 
 SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
-svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 120 bfm
+git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
+cd bfm
+git checkout -b $BFM_RELEASE $BFM_RELEASE
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
