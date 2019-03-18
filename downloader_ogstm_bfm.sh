f02c329f8dc2117bfae811c48cc4f23cc40b373f
Sword <sspada@inogs.it>
2019-03-18 14:18:31 +0100

    Added double quotes to avoid errors due to spaces

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -18,12 +18,12 @@ git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
 cd bfm
 git checkout -b dev_ogs origin/dev_ogs
 
-cd $OGSTM_HOME
+cd "$OGSTM_HOME"
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
 git checkout -b $OGSTM_BRANCH  origin/$OGSTM_BRANCH
 
-cd $OGSTM_HOME
+cd "$OGSTM_HOME"
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
 git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
