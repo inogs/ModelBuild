86b382d8a80f2f7ebd80bcb307418e9910dd2206
Giorgio Bolzon <gbolzon@inogs.it>
2019-03-25 16:46:57 +0100

    setting up MIT version

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -4,8 +4,7 @@
   BFM_version=bfmv5   #  BFMv2 or bfmv5
   BFM_BRANCH=dev_ogs
 
- OGSTM_BRANCH=coupler
- VAR3D_BRANCH=WithVb
+ MITGCM_TAG=checkpoint66j
 
 # ----------- BFM library ---------------------
 
@@ -14,9 +13,11 @@ OGSTM_HOME=$PWD
 # Requirement: to have an account on git server
 git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
 cd bfm
-git checkout -b dev_ogs origin/dev_ogs
+git checkout -b $BFM_BRANCH origin/$BFM_BRANCH
 
 cd $OGSTM_HOME
 git clone git@github.com:gcossarini/BFMCOUPLER.git
 
-# now download MITgcm
+git clone https://github.com/MITgcm/MITgcm.git
+cd MITgcm
+git checkout -b $MITGCM_TAG $MITGCM_TAG
