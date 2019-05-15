551105b32644fcd9afdc2755503606b43aae634f
Giorgio Bolzon <gbolzon@inogs.it>
2019-05-15 11:53:16 +0200

    Using bfmv5 and works on galileo

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
 
-  BFM_version=BFMv2   #  BFMv2 or bfmv5
-  BFM_RELEASE=branches/new_nutupt
-
+  BFM_version=bfmv5  #  BFMv2 or bfmv5
  OGSTM_BRANCH=IscraB_Medsea
 
-SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
+git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
+cd bfm
+git checkout -b dev_ogs origin/dev_ogs
 
-svn co https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} -r 119 bfm
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
 cd ogstm
 git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
-
