346aaaa8ea55ff9bb6d9139e63bfc4f411001e29
Giorgio Bolzon <gbolzon@inogs.it>
2019-05-27 11:32:04 +0200

    Working version of bfmv5 var reduction, there are two calls of bfm_configure.sh, one to generate code and the second one to compile it. The part of code between them has been fixed.
    We use two temporary directories ORIG/ and REDUCED/ containing  BFM_var_list.h and BFM1D_Output_Ecology.F90 - just to check reduction
    
    Once generated_bfmfiles_reducer.py has been executed, files are copied where bfm_configure.sh expect to find them.
    bfm_configure.sh is called with -v, i.e verbosely.

--- builder_ogstm_bfm.sh
+++ builder_ogstm_bfm.sh
@@ -121,19 +121,21 @@ else
    # in-place replace the entire ARCH line
    sed -i "s/.*ARCH.*/        ARCH    = '$INC_FILE'  /"  build/configurations/OGS_PELAGIC/configuration
    cd $BFMDIR/build
-   ./bfm_configure.sh -g -p OGS_PELAGIC
+   ./bfm_configure.sh -gv -o ../lib/libbfm.a -p OGS_PELAGIC
+
    if [ $? -ne 0 ] ; then  echo  ERROR in code generation; exit 1 ; fi
 
-   INPUTDIR=tmp/OGS_PELAGIC/
-   OUTDIR=$INPUTDIR/REDUCED
-   mkdir -p $OUTDIR
+   INPUTDIR=tmp/OGS_PELAGIC/ORIG
+   OUTDIR=tmp/OGS_PELAGIC/REDUCED
+   mkdir -p $INPUTDIR $OUTDIR
    xmlfile=${OGSTMDIR}/bfmv5/BFMtab.xml
    python generated_bfmfiles_reducer.py -i $INPUTDIR -o $OUTDIR -f $xmlfile
    if [ $? -ne 0 ] ; then  echo  ERROR in reduction; exit 1 ; fi
    cp $OUTDIR/BFM_var_list.h ../include/
    cp $OUTDIR/BFM1D_Output_Ecology.F90  ../src/ogstm/
+   cp $OUTDIR/BFM1D_Output_Ecology.F90 tmp/OGS_PELAGIC/
 
-   ./bfm_configure.sh -c -o ../lib/libbfm.a -p OGS_PELAGIC
+   ./bfm_configure.sh -cv -o ../lib/libbfm.a -p OGS_PELAGIC
    if [ $? -ne 0 ] ; then  echo compiling ERROR; exit 1 ; fi
 fi
 
