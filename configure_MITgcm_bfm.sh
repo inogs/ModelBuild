cb034bcab49428f54ed11888b21f25464a7ad5c8
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2020-05-27 16:08:35 +0200

    Minor bug fix about ptracers name & code reordering

--- configure_MITgcm_bfm.sh
+++ configure_MITgcm_bfm.sh
@@ -1,23 +1,33 @@
 #! /bin/bash
 
-COUPLERDIR=$PWD/BFMCOUPLER
-BFMDIR=$PWD/bfm
+#
+#  generates two output directories:
+#  MYCODE
+#  READY_FOR_MODEL_NAMELISTS
+
+
+
+ COUPLERDIR=$PWD/BFMCOUPLER
+     BFMDIR=$PWD/bfm
 MITGCM_ROOT=$PWD/MITgcm
-MYCODE=$PWD/MYCODE
-READY_FOR_MODEL=$PWD/READY_FOR_MODEL_NAMELISTS
+     MYCODE=$PWD/MYCODE
+  NAMELISTS=$PWD/READY_FOR_MODEL_NAMELISTS
 
 mkdir -p $MYCODE $READY_FOR_MODEL
 
 [[ -f $MYCODE/OBCS_OPTIONS.h     ]] ||  cp MITgcm/pkg/obcs/OBCS_OPTIONS.h $MYCODE 
-[[ -f $MYCODE/PTRACER_SIZE.h     ]] ||  cp MITgcm/pkg/ptracer/PTRACER_SIZE.h $MYCODE
+[[ -f $MYCODE/PTRACERS_SIZE.h    ]] ||  cp MITgcm/pkg/ptracers/PTRACERS_SIZE.h $MYCODE
 [[ -f $MYCODE/DIAGNOSTICS_SIZE.h ]] ||  cp MITgcm/pkg/diagnostics/DIAGNOSTICS_SIZE.h $MYCODE
 
 
 
-echo "Now edit and configure you setup in USERCODE"
+echo "Now edit and configure your setup in MYCODE/"
 
 cd $COUPLERDIR
 python bfm_config_gen.py -i $BFMDIR/build/tmp/OGS_PELAGIC/namelist.passivetrc --type code     -o $MYCODE
-python bfm_config_gen.py -i $BFMDIR/build/tmp/OGS_PELAGIC/namelist.passivetrc --type namelist -o $READY_FOR_MODEL
+python bfm_config_gen.py -i $BFMDIR/build/tmp/OGS_PELAGIC/namelist.passivetrc --type namelist -o $NAMELISTS
 # cp data.diagnostics data.ptracers
 python diff_apply.py -i $MITGCM_ROOT  -o $MYCODE
+
+
+
