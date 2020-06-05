9ca729bcdfe069bd3ab602cb5e6db8ef3a4c1909
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2020-06-05 18:14:49 +0200

    Working version - temporary access to another dir - manual edit of bfm/include/BFM_var_list.h

--- configure_MITgcm_bfm.sh
+++ configure_MITgcm_bfm.sh
@@ -18,21 +18,31 @@ mkdir -p $MYCODE $NAMELISTS
 [[ -f $MYCODE/SIZE.h             ]] ||  cp MITgcm/model/inc/SIZE.h $MYCODE
 [[ -f $MYCODE/CPP_OPTIONS.h      ]] ||  cp MITgcm/model/inc/CPP_OPTIONS.h $MYCODE
 [[ -f $MYCODE/OBCS_OPTIONS.h     ]] ||  cp MITgcm/pkg/obcs/OBCS_OPTIONS.h $MYCODE 
-[[ -f $MYCODE/RBCS_OPTIONS.h     ]] ||  cp MITgcm/pkg/rbcs/RBCS_OPTIONS.h $MYCODE 
+[[ -f $MYCODE/RBCS_SIZE.h        ]] ||  cp MITgcm/pkg/rbcs/RBCS_SIZE.h $MYCODE 
 [[ -f $MYCODE/EXF_OPTIONS.h      ]] ||  cp MITgcm/pkg/exf/EXF_OPTIONS.h $MYCODE 
 [[ -f $MYCODE/GCHEM.h            ]] ||  cp MITgcm/pkg/gchem/GCHEM.h $MYCODE 
 [[ -f $MYCODE/GCHEM_OPTIONS.h    ]] ||  cp MITgcm/pkg/gchem/GCHEM_OPTIONS.h $MYCODE 
 [[ -f $MYCODE/PTRACERS_SIZE.h    ]] ||  cp MITgcm/pkg/ptracers/PTRACERS_SIZE.h $MYCODE
 [[ -f $MYCODE/DIAGNOSTICS_SIZE.h ]] ||  cp MITgcm/pkg/diagnostics/DIAGNOSTICS_SIZE.h $MYCODE
 
+# temporary access to code_NADRI
 
-echo "Now edit and configure your setup in MYCODE/"
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/SIZE.h_020p $MYCODE/SIZE.h
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/CPP_OPTIONS.h $MYCODE
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/OBCS_OPTIONS.h $MYCODE 
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/RBCS_SIZE.h $MYCODE 
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/EXF_OPTIONS.h_ALADIN $MYCODE/EXF_OPTIONS.h
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/PTRACERS_SIZE.h $MYCODE/PTRACERS_SIZE.h
+cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/DIAGNOSTICS_SIZE.h $MYCODE/DIAGNOSTICS_SIZE.h
+
+echo "Now edit and configure your setup in $MYCODE/"
+
+cp $COUPLERDIR/BFMcoupler*.F $MYCODE
+cp $COUPLERDIR/BFMcoupler*.h $MYCODE
 
 cd $COUPLERDIR
 python bfm_config_gen.py -i $BFMDIR/build/tmp/OGS_PELAGIC/namelist.passivetrc --type code     -o $MYCODE
 python bfm_config_gen.py -i $BFMDIR/build/tmp/OGS_PELAGIC/namelist.passivetrc --type namelist -o $NAMELISTS
-# cp data.diagnostics data.ptracers
+
 python diff_apply.py -i $MITGCM_ROOT  -o $MYCODE
-
-
 
