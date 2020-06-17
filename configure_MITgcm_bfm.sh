0fe0694b6fc54e9d3c678cb1b7d679a2e58707a0
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2020-06-17 17:43:20 +0200

    Using preset instead of external dirs

--- configure_MITgcm_bfm.sh
+++ configure_MITgcm_bfm.sh
@@ -6,14 +6,17 @@
 #  READY_FOR_MODEL_NAMELISTS
 
 
-
+  
  COUPLERDIR=$PWD/BFMCOUPLER
      BFMDIR=$PWD/bfm
 MITGCM_ROOT=$PWD/MITgcm
      MYCODE=$PWD/MYCODE
+     PRESET=NORTH_ADRIATIC
   NAMELISTS=$PWD/READY_FOR_MODEL_NAMELISTS
 
+
 mkdir -p $MYCODE $NAMELISTS
+#########  copying from MITgcm original code ############
 
 [[ -f $MYCODE/SIZE.h             ]] ||  cp MITgcm/model/inc/SIZE.h $MYCODE
 [[ -f $MYCODE/CPP_OPTIONS.h      ]] ||  cp MITgcm/model/inc/CPP_OPTIONS.h $MYCODE
@@ -25,15 +28,8 @@ mkdir -p $MYCODE $NAMELISTS
 [[ -f $MYCODE/PTRACERS_SIZE.h    ]] ||  cp MITgcm/pkg/ptracers/PTRACERS_SIZE.h $MYCODE
 [[ -f $MYCODE/DIAGNOSTICS_SIZE.h ]] ||  cp MITgcm/pkg/diagnostics/DIAGNOSTICS_SIZE.h $MYCODE
 
-# temporary access to code_NADRI
-
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/SIZE.h_020p $MYCODE/SIZE.h
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/CPP_OPTIONS.h $MYCODE
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/OBCS_OPTIONS.h $MYCODE 
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/RBCS_SIZE.h $MYCODE 
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/EXF_OPTIONS.h_ALADIN $MYCODE/EXF_OPTIONS.h
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/PTRACERS_SIZE.h $MYCODE/PTRACERS_SIZE.h
-cp /gpfs/work/OGS20_PRACE_P/CADEAU/CODE/code_NADRI/DIAGNOSTICS_SIZE.h $MYCODE/DIAGNOSTICS_SIZE.h
+########  copying from preset #############
+cp $PWD/preset/${PRESET}/*.h $MYCODE
 
 echo "Now edit and configure your setup in $MYCODE/"
 
