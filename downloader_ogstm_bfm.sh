a1c04f378255a34ce57cc963f21f999acfcd403f
Giorgio Bolzon <gbolzon@inogs.it>
2021-05-25 16:13:22 +0200

    Change of policy: ogstm is on github  + minor cleaning

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,29 +1,21 @@
 #! /bin/bash
 
 
-  BFM_version=bfmv5   #  BFMv2 or bfmv5
-
  OGSTM_BRANCH=Multivariate
  VAR3D_BRANCH=Multivariate
 
-SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
-# ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
-if  [ $BFM_version ==  BFMv2 ] ; then
 
-svn co --username $SVN_USER https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} bfm
+# Requirement: to have an account on git server
+git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
+cd bfm
+git checkout -b dev_ogs origin/dev_ogs
 
-else
-    # Requirement: to have an account on git server
-    git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
-    cd bfm
-    git checkout -b dev_ogs origin/dev_ogs
-fi
 
 cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/ogstm.git
+git clone git@github.com:inogs/ogstm.git
 cd ogstm
 git checkout -b $OGSTM_BRANCH origin/$OGSTM_BRANCH
 
