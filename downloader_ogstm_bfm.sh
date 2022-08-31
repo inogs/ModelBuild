a64f22133993f70c684093f9c46fd6e012c9cdd0
Giorgio Bolzon <gbolzon@inogs.it>
2022-08-31 10:28:01 +0200

    Setting code tags and OCEANVAR=true

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,35 +1,39 @@
 #! /bin/bash
 
 
- OGSTM_BRANCH=bioptimod_merge
- VAR3D_BRANCH=Multivariate
+ OGSTM_RELEASE=release-4.6
+ BFM_RELEASE=ogs_release-5.2.0
+ VAR3D_RELEASE=release-4.1
+ OASIM_RELEASE=release-1.0
 
-SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
+
 # ----------- BFM library ---------------------
 
 OGSTM_HOME=$PWD
 
 # Requirement: to have an account on git server
-git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
+git clone git@github.com:BFM-Community/BiogeochemicalFluxModel.git bfm
 cd bfm
-git checkout -b dev_ogs_bioptimod origin/dev_ogs_bioptimod
+git checkout -b $BFM_RELEASE $BFM_RELEASE
 
 cd $OGSTM_HOME
 git clone git@github.com:inogs/ogstm.git
 cd ogstm
-git checkout $OGSTM_BRANCH
+git checkout $OGSTM_RELEASE $OGSTM_RELEASE
 
 cd $OGSTM_HOME
 git clone git@github.com:BIOPTIMOD/Forward_Adjoint.git
+cd Forward_Adjoint
+git checkout $OASIM_RELEASE $OASIM_RELEASE
 
 cd $OGSTM_HOME
 git clone git@github.com:pogmat/OASIM-experiments.git OASIM
 cd OASIM
-git checkout ogstm-integration
+git checkout $OASIM_RELEASE $OASIM_RELEASE
 
-exit 0
 
 cd $OGSTM_HOME
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
+git checkout  $VAR3D_RELEASE $VAR3D_RELEASE
+
