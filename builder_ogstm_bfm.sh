dc4184b64c1679937463337244d2ccb1a2f5bfcb
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-09-23 18:32:56 +0200

    Streamline downloader and builder, make them work on m100

--- builder_ogstm_bfm.sh
+++ builder_ogstm_bfm.sh
@@ -1,30 +1,27 @@
-#! /bin/bash
+#! /usr/bin/env bash 
 
-#         builder_ogstm_bfm.sh
-
-#      Edit sections 1,2,3,4 in order to configure compilation and linking.
+# builder_ogstm_bfm.sh
+# Edit sections 1,2,3,4 in order to configure compilation and linking.
 
 ################################################################### 
-#  Section 1. Choose the *.inc file, with the definition of compiler flags, to be included in Makefile
+# Section 1. Choose the *.inc file, with the definition of compiler flags, to be included in Makefile
 #             
-#             This is a machine dependent operation, flags for
-#             most popular compilers (gnu, intel, xl) are provided.
-#             In the following example user will select the file x86_64.LINUX.intel.dbg.inc
-#             both in bfm/compilers/ and ogstm/compilers 
+#    This is a machine dependent operation, flags for
+#    most popular compilers (gnu, intel, xl) are provided.
+#    In the following example user will select the file x86_64.LINUX.intel.dbg.inc
+#    both in bfm/compilers/ and ogstm/compilers 
 
-OGSTM_ARCH=x86_64
-OGSTM_OS=LINUX
-OGSTM_COMPILER=intel
+OGSTM_ARCH=$(uname -m)
+OGSTM_OS=$(uname -s | tr [:lower:] [:upper:])
+CMAKE=1
 DEBUG=       # this is the choice for production flags 
 #DEBUG=.dbg   # this is the one for debug flags
 
-
 ################################################################### 
-#  Section 2. Use of OpenMP threads, to improve the parallelization of ogstm.
-# Just comment one of thes lines:
+# Section 2. Use of OpenMP threads, to improve the parallelization of ogstm.
+#    Just comment one of thes lines:
 export OPENMP_FLAG=-fopenmp  # OpenMP activated
 export OPENMP_FLAG=          # OpenMP deactivated
-
 
 ###################################################################
 # Section 3.  Module loads (and set of environment variables)
@@ -34,56 +31,44 @@ export OPENMP_FLAG=          # OpenMP deactivated
 # Warning : this choice must be consistent with Section 1. 
 
 # Just comment the two following lines you are not using modules. 
-export MODULEFILE=$PWD/ogstm/compilers/machine_modules/g100.intel
+export MODULEFILE=$PWD/ogstm/compilers/machine_modules/m100.hpc-sdk
 source $MODULEFILE
-
 
 ###################################################################
 # Section 4.  Oceanvar inclusion in model
 # Set OCEANVAR=true         to include oceanvar.
 #     DEBUG_OCEANVAR=.dbg   to use debug flags
 
-OCEANVAR=true
+OCEANVAR=false
 DEBUG_OCEANVAR=
 ###################################################################
 
-
-
-
-usage() {
-echo "SYNOPSYS"
-echo "Build BFM and ogstm model"
-echo "builder_ogstm_bfm.sh [ BFMDIR ] [ OGSTMDIR ]"
-echo ""
-echo " Dirs have to be expressed as full paths "
-echo "EXAMPLE"
-echo " ./builder_ogstm_bfm.sh $PWD/bfm $PWD/ogstm "
-
-}
-
-if [ $# -eq 1 ] || [ $# -gt 2 ]; then
-   usage
-   exit 1
+if [[ $# -eq 1 ]] || [[ $# -gt 2 ]]
+then
+    echo "SYNOPSYS"
+    echo "Build BFM and ogstm model"
+    echo "builder_ogstm_bfm.sh [ BFMDIR ] [ OGSTMDIR ]"
+    echo ""
+    echo " Dirs have to be expressed as full paths "
+    echo "EXAMPLE"
+    echo " ./builder_ogstm_bfm.sh $PWD/bfm $PWD/ogstm "}
+    exit 1
 fi
 
-if [ $# -eq 2 ] ; then
-   BFMDIR=$1
-   OGSTMDIR=$2
-else
-   BFMDIR=$PWD/bfm
-   OGSTMDIR=$PWD/ogstm
+if [[ $# -eq 2 ]] 
+then
+    BFMDIR=$1
+    OGSTMDIR=$2
+else 
+    BFMDIR=$PWD/bfm
+    OGSTMDIR=$PWD/ogstm
 fi
 
-
-
-
-
-
-
-# -------------- 3d_var _____
-if [ $OCEANVAR == true ] ; then
+# -------------- 3d_var ---- 
+if [[ $OCEANVAR == true ]]
+then
    cd 3DVar
-   INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${OGSTM_COMPILER}${DEBUG_OCEANVAR}.inc
+   INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${FC}${DEBUG_OCEANVAR}.inc
    cp $INC_FILE compiler.inc
    # make clean
    gmake
@@ -101,22 +86,19 @@ else
    BFMversion=bfmv5
 fi
 
-INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${OGSTM_COMPILER}${DEBUG}.inc
-
-if [ $BFMversion == BFMv2 ] ; then
-   cd ${BFMDIR}/compilers
-   cp $INC_FILE compiler.inc
-
-   ###################################################### just because R1.3 does not have include/
-   mkdir -p  ${BFMDIR}/include
-
-   cd ${BFMDIR}/build
-   ./config_BFM.sh -a ${OGSTM_ARCH} -c ogstm
+INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${FC}${DEBUG}.inc 
+if [[ $BFMversion == BFMv2 ]] 
+then 
+    cd ${BFMDIR}/compilers cp $INC_FILE compiler.inc 
+    # just because R1.3 does not have include/ 
+    mkdir -p  ${BFMDIR}/include 
+    cd ${BFMDIR}/build 
+    ./config_BFM.sh -a ${OGSTM_ARCH} -c ogstm
    cd BLD_OGSTMBFM
    # make clean
    gmake
-
 else
+   echo "Skipping BFM"
    # in-place replace the entire ARCH line
    sed -i "s/.*ARCH.*/        ARCH    = '$INC_FILE'  /"  build/configurations/OGS_PELAGIC/configuration
    cd $BFMDIR/build
@@ -127,35 +109,29 @@ fi
 export BFM_INC=${BFMDIR}/include
 export BFM_LIB=${BFMDIR}/lib
 
-
-CMAKE=1
-
 cd $OGSTMDIR/..
-if [ $CMAKE -eq 1 ] ; then
-  # ------------ new OGSTM builder based on cmake -----------------
-        if [[ $DEBUG == .dbg ]] ; then
-           CMAKE_BUILD_TYPE=Debug
-           OGSTM_BLD_DIR=OGSTM_BUILD_DBG
-        else
-           CMAKE_BUILD_TYPE=Release
-           OGSTM_BLD_DIR=OGSTM_BUILD
-        fi
+if [[ $CMAKE -eq 1 ]]
+then
+    # ------------ new OGSTM builder based on cmake -----------------
+    if [[ $DEBUG == .dbg ]]
+    then
+       CMAKE_BUILD_TYPE=Debug
+       OGSTM_BLD_DIR=OGSTM_BUILD_DBG
+    else
+       CMAKE_BUILD_TYPE=Release
+       OGSTM_BLD_DIR=OGSTM_BUILD
+    fi
 	export BFM_INCLUDE=$BFM_INC
 	export BFM_LIBRARY=$BFM_LIB
 
 	
 	mkdir -p $OGSTM_BLD_DIR
 	cd $OGSTM_BLD_DIR
+    CMAKE_COMMONS="-DMPIEXEC_EXECUTABLE=$(which mpiexec) -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "
+    CMAKE_COMMONS+="-DNETCDF_INCLUDES_C=$NETCDF_INC -DNETCDF_LIBRARIES_C=$NETCDF_LIB/libnetcdf.so -DNETCDFF_INCLUDES_F90=$NETCDFF_INC -DNETCDFF_LIBRARIES_F90=$NETCDFF_LIB/libnetcdff.so -D${BFMversion}=ON"
 
-    if [ $OGSTM_COMPILER == intel ] ; then
-    CMAKE_COMMONS=" -DCMAKE_VERBOSE_MAKEFILE=ON -DMPI_Fortran_COMPILER=mpiifort -DCMAKE_C_COMPILER=icc -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "
-    else
-    CMAKE_COMMONS=" -DCMAKE_VERBOSE_MAKEFILE=ON -DMPI_Fortran_COMPILER=mpif90 -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "
-    fi
-    CMAKE_COMMONS+=" -DNETCDF_INCLUDES_C=$NETCDF_INC -DNETCDF_LIBRARIES_C=$NETCDF_LIB/libnetcdf.so -DNETCDFF_INCLUDES_F90=$NETCDFF_INC -DNETCDFF_LIBRARIES_F90=$NETCDFF_LIB/libnetcdff.so"
-    CMAKE_COMMONS+=" -D${BFMversion}=ON"
-
-	if [ $OCEANVAR == true ] ; then
+	if [[ $OCEANVAR == true ]]
+    then
 	    export DA_INCLUDE=$DA_INC
 	    export DA_LIBRARY=$DA_INC
 	    export PETSC_LIB=$PETSC_LIB/libpetsc.so
@@ -172,10 +148,9 @@ if [ $CMAKE -eq 1 ] ; then
     make
 else
 
-  # ----------- standard OGSTM builder -----------------
-
+    # ----------- standard OGSTM builder -----------------
 	cd ${OGSTMDIR}/compilers
-	INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${OGSTM_COMPILER}${DEBUG}.inc
+	INC_FILE=${OGSTM_ARCH}.${OGSTM_OS}.${FC}${DEBUG}.inc
 	cp $INC_FILE compiler.inc
 	
 	cd ${OGSTMDIR}/build
@@ -188,23 +163,19 @@ else
 
 fi
 
-
-if [ $? -ne 0 ] ; then  echo  ERROR; exit 1 ; fi
-
 ### OGSTM NAMELIST GENERATION (also by Frequency Control )
-
 mkdir -p ${OGSTMDIR}/ready_for_model_namelists/
 
-if [ $BFMversion == bfmv5 ] ; then
-   cp ${BFMDIR}/build/tmp/OGS_PELAGIC/namelist.passivetrc ${OGSTMDIR}/bfmv5/
-   cd ${OGSTMDIR}/bfmv5/
-   ./ogstm_namelist_gen.py #generates namelist.passivetrc_new
+if [[ $BFMversion == bfmv5 ]]
+then
+    cp ${BFMDIR}/build/tmp/OGS_PELAGIC/namelist.passivetrc ${OGSTMDIR}/bfmv5/
+    cd ${OGSTMDIR}/bfmv5/
+    ./ogstm_namelist_gen.py #generates namelist.passivetrc_new
 
-   cp ${OGSTMDIR}/src/namelists/namelist*    ${OGSTMDIR}/ready_for_model_namelists/ 
-   cp namelist.passivetrc_new                ${OGSTMDIR}/ready_for_model_namelists/namelist.passivetrc #overwriting
-   cp ${BFMDIR}/build/tmp/OGS_PELAGIC/*.nml  ${OGSTMDIR}/ready_for_model_namelists/
+    cp ${OGSTMDIR}/src/namelists/namelist*    ${OGSTMDIR}/ready_for_model_namelists/ 
+    cp namelist.passivetrc_new                ${OGSTMDIR}/ready_for_model_namelists/namelist.passivetrc #overwriting
+    cp ${BFMDIR}/build/tmp/OGS_PELAGIC/*.nml  ${OGSTMDIR}/ready_for_model_namelists/
 else
-   #V2
    cp ${OGSTMDIR}/src/namelists/namelist*    ${OGSTMDIR}/ready_for_model_namelists/
    cp ${BFMDIR}/src/namelist/*.nml           ${OGSTMDIR}/ready_for_model_namelists/
 fi
--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,33 +1,47 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
+# General settings
+ROOT="$(dirname -- "${BASH_SOURCE[0]}")"
 
-  BFM_version=bfmv5   #  BFMv2 or bfmv5
-  BFM_RELEASE=branches/pl_mod 
+# BFM settings
 
- OGSTM_BRANCH=master
- VAR3D_BRANCH=Multivariate
+BFM_PATH=bfm
+BFM_VERSION=v5
 
-SVN_USER=svnogs01  # user on https://hpc-forge.cineca.it/
-# ----------- BFM library ---------------------
+# BFMv2 settings
+# User on https://hpc-forge.cineca.it/
+BFMv2_USER=svnogs01
+BFMv2_REPO=https://hpc-forge.cineca.it/svn/BFMv2
+BFMv2_RELEASE=branches/pl_mod 
 
-OGSTM_HOME=$PWD
+# BFMv5 settings
+# Requirement: to have an account on git server
+BFMv5_REPO=git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git
+BFMv5_BRANCH=release_5.3
 
-if  [ $BFM_version ==  BFMv2 ] ; then
+# OGSTM settings
+OGSTM_REPO=git@github.com:inogs/ogstm.git
+OGSTM_BRANCH=release_4.5
+OGSTM_PATH=ogstm
 
-svn co --username $SVN_USER https://hpc-forge.cineca.it/svn/${BFM_version}/${BFM_RELEASE} bfm
+# VAR3D settings
+VAR3D_REPO=git@gitlab.hpc.cineca.it:OGS/3DVar.git
+VAR3D_BRANCH=Multivariate
+VAR3D_PATH=3DVar
 
+cd -- "${ROOT}"
+
+echo -e "\n==== Downloading BFM ===="
+if  [[ "${BFM_VERSION}" ==  v2 ]]; then
+  svn co --username "${BFMv2_USER}" "${BFMv2_REPO}/${BFMv2_RELEASE}" "${BFM_PATH}"
+elif [[ "${BFM_VERSION}" == v5 ]]; then
+  git clone -b "${BFMv5_BRANCH}" "${BFMv5_REPO}" "${BFM_PATH}" 
 else
-    # Requirement: to have an account on git server
-    git clone git@github.com:CMCC-Foundation/BiogeochemicalFluxModel.git bfm
-    cd bfm
-    git checkout -b dev_ogs origin/dev_ogs
-fi
+  echo "BFM version not recognized or supported."
+fi || echo "An error occurred while cloning BFM. Skipping."
 
-cd $OGSTM_HOME
-git clone git@github.com:inogs/ogstm.git
+echo -e "\n==== Downloading OGSTM ===="
+git clone -b "${OGSTM_BRANCH}" "${OGSTM_REPO}" "${OGSTM_PATH}" || echo "An error occurred while cloning OGSTM. Skipping"
 
-
-cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
-cd 3DVar
-git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
+echo -e "\n==== Downloading 3DVar ===="
+git clone -b "${VAR3D_BRANCH}" "${VAR3D_REPO}" "${VAR3D_PATH}" || echo "An error occurred while cloning 3DVar. Skipping"
