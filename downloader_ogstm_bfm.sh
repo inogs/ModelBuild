f969b3c5ef2bec34f6f987c63759955874a77a05
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2022-10-11 11:38:08 +0200

    Use --single-branch option within downloader git-clones

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -35,13 +35,13 @@ echo -e "\n==== Downloading BFM ===="
 if  [[ "${BFM_VERSION}" ==  v2 ]]; then
   svn co --username "${BFMv2_USER}" "${BFMv2_REPO}/${BFMv2_RELEASE}" "${BFM_PATH}"
 elif [[ "${BFM_VERSION}" == v5 ]]; then
-  git clone -b "${BFMv5_BRANCH}" "${BFMv5_REPO}" "${BFM_PATH}" 
+  git clone --single-branch -b "${BFMv5_BRANCH}" "${BFMv5_REPO}" "${BFM_PATH}" 
 else
   echo "BFM version not recognized or supported."
 fi || echo "An error occurred while cloning BFM. Skipping."
 
 echo -e "\n==== Downloading OGSTM ===="
-git clone -b "${OGSTM_BRANCH}" "${OGSTM_REPO}" "${OGSTM_PATH}" || echo "An error occurred while cloning OGSTM. Skipping"
+git clone --single-branch -b "${OGSTM_BRANCH}" "${OGSTM_REPO}" "${OGSTM_PATH}" || echo "An error occurred while cloning OGSTM. Skipping"
 
 echo -e "\n==== Downloading 3DVar ===="
-git clone -b "${VAR3D_BRANCH}" "${VAR3D_REPO}" "${VAR3D_PATH}" || echo "An error occurred while cloning 3DVar. Skipping"
+git clone --single-branch -b "${VAR3D_BRANCH}" "${VAR3D_REPO}" "${VAR3D_PATH}" || echo "An error occurred while cloning 3DVar. Skipping"
