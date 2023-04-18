20fce64a1c43223bcadf28425b3ef71a0d3e1384
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2023-04-18 16:05:32 +0200

    add options to skip bfm/ogstm compilation, increase verbosity, and avoid clearing caches

--- build.sh
+++ build.sh
@@ -17,10 +17,14 @@ DESCRIPTION
     Download options
         --no-download                           Do not download the source code from GitHub
         --clone-options     GIT_CLONE_OPTIONS   git-clone options (such as --single-branch)
-        --ogstm-branch      OGSTM_BRANCH        Which branch of OGSTM tree to use (relative to the location of $0, default dev_gpu)
-        --bfm-branch        BFM_BRANCH          Which branch of BFM tree to use(relative to the location of $0, default dev_gpu)
-        --var3d-branch      VAR3D_BRANCH        Which branch of 3DVar tree to use ( source (relative to the location of $0, default dev_gpu)
+        --ogstm-branch      OGSTM_BRANCH        Which branch of OGSTM tree to use (default dev_gpu)
+        --bfm-branch        BFM_BRANCH          Which branch of BFM tree to use (default dev_gpu)
+        --var3d-branch      VAR3D_BRANCH        Which branch of 3DVar tree to use (default dev_gpu)
     Compilation options
+        --verbose                               Increases verbosity
+        --fast                                  Avoids clearing cache/invoking cmake
+        --skip-bfm                              Disables BFM building
+        --skip-ogstm                            Disables OGSTM building
         --debug                                 Compiles debug version
         --var3d                                 Enables data assimilation component
         --machine-modules   MOD_NAME            Specifies which file (among the available ones in OGSTM) to source, to load modules
@@ -32,7 +36,7 @@ DESCRIPTION
 EOF
 }
 
-LONGOPTS='help,no-download,debug,var3d,clone-options:,var3d-path:,var3d-branch:,bfm-path:,bfm-branch:,ogstm-path:,ogstm-branch:,module-file:,build-path:'
+LONGOPTS='help,no-download,debug,verbose,fast,skip-bfm,skip-ogstm,var3d,clone-options:,var3d-path:,var3d-branch:,bfm-path:,bfm-branch:,ogstm-path:,ogstm-branch:,module-file:,build-path:'
 ARGS=$(getopt --options '' --longoptions ${LONGOPTS} -- "${@}")
 if [[ $? -ne 0 ]]; then
         usage
@@ -45,6 +49,10 @@ DEBUG=false
 VAR3D=false
 MOD_NAME=m100.hpc-sdk
 BUILD_PATH="${ROOT}/OGSTM_BUILD"
+VERBOSE=false
+BUILD_BFM=true
+BUILD_OGSTM=false
+CLEAR_CACHE=true
 
 # BFM settings
 BFM_PATH="${ROOT}/bfm"
@@ -64,6 +72,22 @@ VAR3D_PATH=3DVar
 eval "set -- ${ARGS}"
 while true; do
     case "${1}" in
+        (--verbose)
+            VERBOSE=true
+            shift
+        ;;
+        (--fast)
+            CLEAR_CACHE=false
+            shift
+        ;;
+        (--skip-bfm)
+            BUILD_BFM=false
+            shift
+        ;;
+        (--skip-ogstm)
+            BUILD_OGSTM=false
+            shift
+        ;;
         (--debug)
             DEBUG=true
             DEBUG_SUFFIX=.dbg
@@ -142,66 +166,86 @@ fi
 echo -e "\n==== Sourcing ${MOD_NAME} ===="
 source "$ROOT/ogstm/compilers/machine_modules/${MOD_NAME}" || :
 
-echo -e "\n==== Building BFM ===="
-export BFM_INC=${BFM_PATH}/include
-export BFM_LIB=${BFM_PATH}/lib
-cd "${BFM_PATH}/build" || exit
-./bfm_configure.sh -gcfv -o ../lib/libbfm.a -p OGS_PELAGIC -a ${ARCH}.${OS}.${FC}${DEBUG_SUFFIX}.inc
-
-echo -e "\n==== Building OGSTM ===="
-export BFM_INCLUDE=$BFM_INC
-export BFM_LIBRARY=$BFM_LIB
-mkdir -p "${BUILD_PATH}"
-cd "${BUILD_PATH}" || exit
-
-if [[ $DEBUG == true ]]; then
-    CMAKE_BUILD_TYPE=Debug
-else
-    CMAKE_BUILD_TYPE=Release
+if [[ $BUILD_BFM == true ]]; then
+    echo -e "\n==== Building BFM ===="
+    export BFM_INC=${BFM_PATH}/include
+    export BFM_LIB=${BFM_PATH}/lib
+    cd "${BFM_PATH}/build" || exit
+    
+    if [[ $CLEAR_CACHE == true ]]; then
+        BFM_OPTIONS="-gc"
+    else
+        BFM_OPTIONS="-fc"
+    fi
+    if [[ $VERBOSE == true ]]; then
+        BFM_OPTIONS="${BFM_OPTIONS}v"
+    fi
+    ./bfm_configure.sh ${BFM_OPTIONS} -o ../lib/libbfm.a -p OGS_PELAGIC -a ${ARCH}.${OS}.${FC}${DEBUG_SUFFIX}.inc
 fi
-CMAKE_COMMONS="-DCMAKE_VERBOSE_MAKEFILE=ON "
-CMAKE_COMMONS+="-DMPIEXEC_EXECUTABLE=$(which mpiexec) "
-if [[ $MOD_NAME == m100.hpc-sdk ]]; then
-    CMAKE_COMMONS+="-DCMAKE_C_COMPILER_ID=PGI "
-    CMAKE_COMMONS+="-DCMAKE_Fortran_COMPILER_ID=PGI "
-    CMAKE_COMMONS+="-DMPI_C_COMPILER=$(which mpipgicc) "
-    CMAKE_COMMONS+="-DMPI_Fortran_COMPILER=$(which mpipgifort) "
-elif [[ $MOD_NAME == m100.gnu ]]; then
-    CMAKE_COMMONS+="-DCMAKE_C_COMPILER_ID=GNU "
-    CMAKE_COMMONS+="-DCMAKE_Fortran_COMPILER_ID=GNU "
-    CMAKE_COMMONS+="-DMPI_C_COMPILER=$(which mpicc) "
-    CMAKE_COMMONS+="-DMPI_Fortran_COMPILER=$(which mpifort) "
-fi
-CMAKE_COMMONS+="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "
-CMAKE_COMMONS+="-DNETCDF_INCLUDES_C=${NETCDF_INC} "
-CMAKE_COMMONS+="-DNETCDF_LIBRARIES_C=${NETCDF_LIB}/libnetcdf.so "
-CMAKE_COMMONS+="-DNETCDFF_INCLUDES_F90=${NETCDFF_INC} "
-CMAKE_COMMONS+="-DNETCDFF_LIBRARIES_F90=${NETCDFF_LIB}/libnetcdff.so "
-CMAKE_COMMONS+="-Dbfmv5=ON"
-if [[ $VAR3D == true ]]; then
 
-    if [[ $DOWNLOAD == true ]]; then
-        echo -e "\n==== Downloading 3DVar ===="
-        git clone ${GIT_CLONE_OPTIONS} --branch ${VAR3D_BRANCH} -- ${VAR3D_REPO} "${VAR3D_PATH}" || echo "An error occurred while cloning 3DVar. Skipping"
+if [[ $BUILD_OGSTM == true ]]; then
+    echo -e "\n==== Building OGSTM ===="
+    export BFM_INCLUDE=$BFM_INC
+    export BFM_LIBRARY=$BFM_LIB
+    mkdir -p "${BUILD_PATH}"
+    cd "${BUILD_PATH}" || exit
+
+    if [[ $DEBUG == true ]]; then
+        CMAKE_BUILD_TYPE=Debug
+    else
+        CMAKE_BUILD_TYPE=Release
+    fi
+    CMAKE_COMMONS="-DCMAKE_VERBOSE_MAKEFILE=ON "
+    CMAKE_COMMONS+="-DMPIEXEC_EXECUTABLE=$(which mpiexec) "
+    if [[ $MOD_NAME == m100.hpc-sdk ]]; then
+        CMAKE_COMMONS+="-DCMAKE_C_COMPILER_ID=PGI "
+        CMAKE_COMMONS+="-DCMAKE_Fortran_COMPILER_ID=PGI "
+        CMAKE_COMMONS+="-DMPI_C_COMPILER=$(which mpipgicc) "
+        CMAKE_COMMONS+="-DMPI_Fortran_COMPILER=$(which mpipgifort) "
+    elif [[ $MOD_NAME == m100.gnu ]]; then
+        CMAKE_COMMONS+="-DCMAKE_C_COMPILER_ID=GNU "
+        CMAKE_COMMONS+="-DCMAKE_Fortran_COMPILER_ID=GNU "
+        CMAKE_COMMONS+="-DMPI_C_COMPILER=$(which mpicc) "
+        CMAKE_COMMONS+="-DMPI_Fortran_COMPILER=$(which mpifort) "
+    fi
+    CMAKE_COMMONS+="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} "
+    CMAKE_COMMONS+="-DNETCDF_INCLUDES_C=${NETCDF_INC} "
+    CMAKE_COMMONS+="-DNETCDF_LIBRARIES_C=${NETCDF_LIB}/libnetcdf.so "
+    CMAKE_COMMONS+="-DNETCDFF_INCLUDES_F90=${NETCDFF_INC} "
+    CMAKE_COMMONS+="-DNETCDFF_LIBRARIES_F90=${NETCDFF_LIB}/libnetcdff.so "
+    CMAKE_COMMONS+="-Dbfmv5=ON"
+    if [[ $VAR3D == true ]]; then
+
+        if [[ $DOWNLOAD == true ]]; then
+            echo -e "\n==== Downloading 3DVar ===="
+            git clone ${GIT_CLONE_OPTIONS} --branch ${VAR3D_BRANCH} -- ${VAR3D_REPO} "${VAR3D_PATH}" || echo "An error occurred while cloning 3DVar. Skipping"
+        fi
+        
+        cd "${VAR3D_PATH}" || exit
+        cp "${ARCH}.${OS}.${FC}${DEBUG_SUFFIX}.inc" compiler.inc
+
+        echo -e "\n==== Building 3DVar ===="
+        make
+        
+        export DA_INCLUDE="${VAR3D_PATH}"
+        export DA_LIBRARY="${VAR3D_PATH}"
+        export PETSC_LIB="${PETSC_LIB}/libpetsc.so"
+        CMAKE_COMMONS+="-DPETSC_LIBRARIES=${PETSC_LIB} "
+        CMAKE_COMMONS+="-DPNETCDF_LIBRARIES=${PNETCDF_LIB}/libpnetcdf.a "
+        cp "${OGSTM_PATH}/DataAssimilation.cmake" "${OGSTM_PATH}/CMakeLists.txt"
+    else
+        cp "${OGSTM_PATH}/GeneralCmake.cmake" "${OGSTM_PATH}/CMakeLists.txt"
     fi
     
-    cd "${VAR3D_PATH}" || exit
-    cp "${ARCH}.${OS}.${FC}${DEBUG_SUFFIX}.inc" compiler.inc
-
-    echo -e "\n==== Building 3DVar ===="
+    if [[ $CLEAR_CACHE == true ]]; then
+        OGSTM_CMAKE_OPTIONS=
+        if [[ $VERBOSE == true ]]; then
+            OGSTM_CMAKE_OPTIONS=-LAH
+        fi
+        cmake ${OGSTM_CMAKE_OPTIONS} ${OGSTM_PATH} ${CMAKE_COMMONS}
+    fi
     make
-    
-    export DA_INCLUDE="${VAR3D_PATH}"
-    export DA_LIBRARY="${VAR3D_PATH}"
-    export PETSC_LIB="${PETSC_LIB}/libpetsc.so"
-    CMAKE_COMMONS+="-DPETSC_LIBRARIES=${PETSC_LIB} "
-    CMAKE_COMMONS+="-DPNETCDF_LIBRARIES=${PNETCDF_LIB}/libpnetcdf.a "
-    cp "${OGSTM_PATH}/DataAssimilation.cmake" "${OGSTM_PATH}/CMakeLists.txt"
-else
-    cp "${OGSTM_PATH}/GeneralCmake.cmake" "${OGSTM_PATH}/CMakeLists.txt"
 fi
-cmake -LAH ${OGSTM_PATH} ${CMAKE_COMMONS}
-make
 
 echo -e "\n==== Generating namelists ===="
 cp "${BFM_PATH}/build/tmp/OGS_PELAGIC/namelist.passivetrc" "${OGSTM_PATH}/bfmv5/"
