8148ac92f9f107d40ec4a91ab8da216885db7e23
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2024-09-23 15:38:57 +0200

    Excluding 3Dvar

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -2,7 +2,6 @@
 
 
  OGSTM_BRANCH=neccton
- VAR3D_RELEASE=release-4.1
  OASIM_RELEASE=release-1.0
 
 # ----------- BFM library ---------------------
@@ -26,12 +25,6 @@ git checkout -b $OASIM_RELEASE $OASIM_RELEASE
 
 cd $OGSTM_HOME
 git clone git@github.com:BIOPTIMOD/OASIM_ATM.git OASIM
-cd OASIM
-#git checkout -b $OASIM_RELEASE $OASIM_RELEASE
 
-cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
-cd 3DVar
-git checkout -b $VAR3D_RELEASE $VAR3D_RELEASE
 
 
