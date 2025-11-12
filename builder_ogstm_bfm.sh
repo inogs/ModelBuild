64df4eb494b6ad4e5ca2abc81ecf61f01054494a
Paolo Lazzari <plazzari@login01.leonardo.local>
2025-11-12 12:22:11 +0100

    setting debug mode

--- builder_ogstm_bfm.sh
+++ builder_ogstm_bfm.sh
@@ -16,7 +16,7 @@ OGSTM_ARCH=x86_64
 OGSTM_OS=LINUX
 OGSTM_COMPILER=intel
 DEBUG=       # this is the choice for production flags 
-#DEBUG=.dbg   # this is the one for debug flags
+DEBUG=.dbg   # this is the one for debug flags
 
 
 ################################################################### 
@@ -121,9 +121,14 @@ module unload cmake
 module load cmake/3.27.7
 
 # Configure and build FABM
+#export FFLAGS='-fcheck=all -O0 -Wall -Wextra -g -cpp -DDEBUG -DQUICK'
+export FFLAGS='-O0 -g -traceback -fp-stack-check -check bounds -fpe0'
+#FABM_ARGS="-DFABM_INSTITUTES=gotm;msi;ogs;spectral -DFABM_OGS_BASE=${REPO_DIR}/extern/ogs -DFABM_SPECTRAL_BASE=${REPO_DIR}/extern/fabm-spectral -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_BUILD_TYPE=release"
+FABM_ARGS="-DFABM_INSTITUTES=gotm;msi;ogs;spectral -DFABM_OGS_BASE=${REPO_DIR}/extern/ogs -DFABM_SPECTRAL_BASE=${REPO_DIR}/extern/fabm-spectral -DCMAKE_Fortran_COMPILER=ifort  -DCMAKE_BUILD_TYPE=debug"
 cmake $OGSTM_HOME/fabm/ \
       -DFABM_HOST=$MODEL_SELECTED \
-      -DCMAKE_INSTALL_PREFIX=$OGSTM_HOME/local/fabm/ogstm_test/   # install into /ModelBuild/local
+      -DCMAKE_INSTALL_PREFIX=$OGSTM_HOME/local/fabm/ogstm_test/ \  # install into /ModelBuild/local
+       $FABM_ARGS
 
 make install
 
