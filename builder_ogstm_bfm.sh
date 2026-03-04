242f7f0c8bf49dedd50bb96cd7f7909457d13b19
Paolo Lazzari <plazzari@login02.leonardo.local>
2026-03-04 18:31:23 +0100

    update fabm driver

--- builder_ogstm_bfm.sh
+++ builder_ogstm_bfm.sh
@@ -117,9 +117,16 @@ cat <<EOL > src/drivers/$MODEL_SELECTED/fabm_driver.h
 #define _FABM_DIMENSION_COUNT_ 3
 #define _FABM_DEPTH_DIMENSION_INDEX_ 1
 #define _FABM_VECTORIZED_DIMENSION_INDEX_ 1
+#define _FABM_MASK_TYPE_ integer(1)
+#define _FABM_UNMASKED_VALUE_ 1
+
+! Specify that the vertical index of the bottom cell is variable (kmax depends on i,j)
+#define _FABM_BOTTOM_INDEX_ -1
 
 #include "fabm.h"
 EOL
+
+#define _FABM_MASK_TYPE_ integer
 
 cd $OGSTM_HOME
 
