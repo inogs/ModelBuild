6050621f665dff427280462db67147cf6c6ee734
Giorgio Bolzon <gbolzon@ogs.it>
2026-03-31 10:06:21 +0200

    starting V13C work with in-situ T in ogsm

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
- OGSTM_BRANCH=bioptimod_merge
+ OGSTM_BRANCH=20-introduction-of-in-situ-temperature
  BFM_RELEASE=ogs_release-5.4.0
  VAR3D_RELEASE=release-4.1
  OASIM_RELEASE=release-1.0
