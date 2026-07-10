a5301126f38fbca60bfce666dd05547773b2197f
Giorgio Bolzon <gbolzon@ogs.it>
2026-07-10 14:04:18 +0200

    Using Multivariate branch temporary

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -3,7 +3,7 @@
 
  OGSTM_BRANCH=28-v13c-quid
  BFM_RELEASE=neccton
- VAR3D_BRANCH=MultiVariate
+ VAR3D_RELEASE=release-4.2 # todo
  OASIM_RELEASE=release-1.0
 
 # ----------- BFM library ---------------------
@@ -33,6 +33,7 @@ git checkout -b $OASIM_RELEASE $OASIM_RELEASE
 cd $OGSTM_HOME
 git clone git@github.com:inogs/3dVarBio.git 3DVar
 cd 3DVar
-git checkout -b $VAR3D_RELEASE $VAR3D_RELEASE
+#git checkout -b $VAR3D_RELEASE $VAR3D_RELEASE
+git switch MultiVariate
 
 
