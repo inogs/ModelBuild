b47b282bd09cedf071225eda8c33491ad33c1638
Giorgio Bolzon <gbolzon@ogs.it>
2026-07-10 13:52:17 +0200

    Taking 3Dvar from github

--- downloader_ogstm_bfm.sh
+++ downloader_ogstm_bfm.sh
@@ -3,7 +3,7 @@
 
  OGSTM_BRANCH=28-v13c-quid
  BFM_RELEASE=neccton
- VAR3D_RELEASE=release-4.1
+ VAR3D_BRANCH=MultiVariate
  OASIM_RELEASE=release-1.0
 
 # ----------- BFM library ---------------------
@@ -31,7 +31,7 @@ cd OASIM
 git checkout -b $OASIM_RELEASE $OASIM_RELEASE
 
 cd $OGSTM_HOME
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
+git clone git@github.com:inogs/3dVarBio.git 3DVar
 cd 3DVar
 git checkout -b $VAR3D_RELEASE $VAR3D_RELEASE
 
